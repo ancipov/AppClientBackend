3a0448724bd443f3563648aa70daec0635133bef
Maxim Balyaba <mbalyaba@gmail.com>
2021-09-27 17:11:13 +0300

    sequelize aliases added

--- routes/notes.js
+++ routes/notes.js
@@ -1,31 +1,15 @@
 const utils = require('../utils');
 const _ = require('lodash');
 const {models} = require('../sequelize');
-const { Op , cast} = require("sequelize");
 
 module.exports = function (app) {
     //find
     app.get('/notes', [utils.jwtMW], async (req, res) => {
         try {
-            const defaultWhere = {
-                userId: utils.getUserId(req)
-            };
-            const requestWhere = JSON.parse(req.query.where || "{}");
-            let resultWhere;
-            if(req.query.where && req.query.where.indexOf("updatedAt") != -1 && req.query.where.indexOf("$gt") != -1){
-                //fetch case, so we need to cast date and use sequelize condition operators
-                const dateValue = cast(requestWhere.updatedAt["$gt"],'DATETIME');
-                resultWhere = {
-                    "updatedAt":{[Op.gte]: dateValue}
-                };
-            } else {
-                resultWhere = {
-                ...defaultWhere,
-                ...requestWhere
-                };
-            }
             const notes = await models.notes.findAll({
-                where: resultWhere,
+                where: {
+                    ...JSON.parse(req.query.where || "{}")
+                },
                 limit: req.query.limit ? Number(req.query.limit) : 1000
             });
             return res.json(notes);
@@ -64,7 +48,6 @@ module.exports = function (app) {
     //create
     app.post('/notes', [utils.jwtMW], async (req, res) => {
         try {
-            req.body.userId = utils.getUserId(req);
             let note = await models.notes.create(req.body);
 
             return res.json(note);
--- sequelize/index.js
+++ sequelize/index.js
@@ -3,26 +3,48 @@ const utils = require('../utils');
 const {Sequelize} = require('sequelize');
 const {applyExtraSetup} = require('./extra-setup');
 
-// const sequelize = new Sequelize(utils.DB.database, utils.DB.user, utils.DB.password, {
-//     host: utils.DB.server,
-//     port: utils.DB.port,
-//     logging: false,
-//     dialect: 'mssql',
-//     driver: 'tedious',
-//     dialectOptions: {
-//         options: {
-//             useUTC: true,
-//             dateFirst: 1,
-//             requestTimeout: 30000,
-//             encrypt: false
-//         }
-//     }
-// });
-
+const Op = Sequelize.Op;
+const operatorsAliases = {
+    $eq: Op.eq,
+    $ne: Op.ne,
+    $gte: Op.gte,
+    $gt: Op.gt,
+    $lte: Op.lte,
+    $lt: Op.lt,
+    $not: Op.not,
+    $in: Op.in,
+    $notIn: Op.notIn,
+    $is: Op.is,
+    $like: Op.like,
+    $notLike: Op.notLike,
+    $iLike: Op.iLike,
+    $notILike: Op.notILike,
+    $regexp: Op.regexp,
+    $notRegexp: Op.notRegexp,
+    $iRegexp: Op.iRegexp,
+    $notIRegexp: Op.notIRegexp,
+    $between: Op.between,
+    $notBetween: Op.notBetween,
+    $overlap: Op.overlap,
+    $contains: Op.contains,
+    $contained: Op.contained,
+    $adjacent: Op.adjacent,
+    $strictLeft: Op.strictLeft,
+    $strictRight: Op.strictRight,
+    $noExtendRight: Op.noExtendRight,
+    $noExtendLeft: Op.noExtendLeft,
+    $and: Op.and,
+    $or: Op.or,
+    $any: Op.any,
+    $all: Op.all,
+    $values: Op.values,
+    $col: Op.col
+};
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: 'db.sqlite'
+    storage: 'db.sqlite',
+    operatorsAliases
 });
 
 (async() => {
